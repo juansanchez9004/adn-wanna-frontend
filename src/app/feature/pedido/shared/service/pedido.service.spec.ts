eea67a1c208baefc6344811733ca3a65c1d87d13
CEIBA\pablo.tabares <pablo.tabares@ceiba.com.co>
2022-05-23 21:57:33 -0500

    Se realizan test unitarios del servicio de pedido y se crea mock para crear las respuestas.

--- src/app/feature/pedido/shared/service/pedido.service.spec.ts
+++ src/app/feature/pedido/shared/service/pedido.service.spec.ts
@@ -1,16 +1,114 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpService } from '@core/services/http.service';
+import { environment } from 'src/environments/environment';
 import { PedidoService } from './pedido.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { PedidoMockService } from '../../../../shared/mock/pedido-mock-service';
+import { HttpResponse } from '@angular/common/http';
 
 describe('PedidoService', () => {
+  let httpMock: HttpTestingController;
   let service: PedidoService;
+  const pedidoMockService: PedidoMockService = new PedidoMockService();
+
+  const apiEndpointCrearOrden = `${environment.endpoint}/pedido`;
+  const apiEndpointConsultarTodosLosClientes = `${environment.endpoint}/cliente/todos`;
+  const apiEndpointConsultarTodosLosProductos = `${environment.endpoint}/producto/todos`;
+  const apiEndpointConsultarTodosLosPedidosPendientes = `${environment.endpoint}/pedido/pendientes`;
+  const apiEndpointEntregarPedido = `${environment.endpoint}/pedido/entregar/2`;
+  const apiEndpointPedidosEntregados = `${environment.endpoint}/pedido/entregados`;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const injector = TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PedidoService, HttpService]
+    });
+    httpMock = injector.inject(HttpTestingController);
     service = TestBed.inject(PedidoService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    const pedidoService: PedidoService = TestBed.inject(PedidoService);
+    expect(pedidoService).toBeTruthy();
   });
+
+  it('deberia crear un pedido', () => {
+    const dummyPedido = pedidoMockService.crearComandoSolicitudOrdenar();
+    
+    service.crearOrden(dummyPedido).subscribe((response) => {
+      expect(response).toBe(10);
+    });
+
+    const req = httpMock.expectOne(apiEndpointCrearOrden);
+    expect(req.request.method).toBe('POST');
+    req.event(new HttpResponse<number>({ body: 10 }));
+  });
+
+  it('deberia consultar todos los clientes', () => {
+
+    const dummyClientes = pedidoMockService.crearListadoClientes();
+    
+    service.consultarTodosLosClientes().subscribe(response => {
+      expect(response.length).toBe(3);
+      expect(response).toBe(dummyClientes);
+    });
+
+    const req = httpMock.expectOne(apiEndpointConsultarTodosLosClientes);
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyClientes);
+  });
+
+  it('deberia consultar todos los productos', () => {
+
+    const dummyProductos = pedidoMockService.crearListadoProductos();
+    
+    service.consultarTodosLosProductos().subscribe(response => {
+      expect(response.length).toBe(2);
+      expect(response).toBe(dummyProductos);
+    });
+
+    const req = httpMock.expectOne(apiEndpointConsultarTodosLosProductos);
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyProductos);
+  });
+
+  it('deberia consultar todos los pedidos pendientes', () => {
+
+    const dummyPedidosPendientes = pedidoMockService.crearListadoPedidosPendientes();
+    
+    service.consultarTodosLosPedidosPendientes().subscribe(response => {
+      expect(response.length).toBe(4);
+      expect(response).toBe(dummyPedidosPendientes);
+    });
+
+    const req = httpMock.expectOne(apiEndpointConsultarTodosLosPedidosPendientes);
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyPedidosPendientes);
+  });
+
+  it('deberia entregar un pedido', () => {
+    const dummyIdPedido = 2;
+    
+    service.entregarPedido(dummyIdPedido).subscribe((response) => {
+      expect(response).toBeNull();
+    });
+
+    const req = httpMock.expectOne(apiEndpointEntregarPedido);
+    expect(req.request.method).toBe('POST');
+    req.event(new HttpResponse<number>({ body: null }));
+  });
+
+  it('deberia consultar todos los pedidos entregados', () => {
+
+    const dummyPedidosEntregados = pedidoMockService.crearListadoPedidosEntregados();
+    
+    service.consultarTodosLosPedidosEntregados().subscribe(response => {
+      expect(response.length).toBe(3);
+      expect(response).toBe(dummyPedidosEntregados);
+    });
+
+    const req = httpMock.expectOne(apiEndpointPedidosEntregados);
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyPedidosEntregados);
+  });  
 });
---
+++ src/app/shared/mock/pedido-mock-service.ts
@@ -0,0 +1,108 @@
+export class PedidoMockService {
+  crearComandoSolicitudOrdenar() {
+    return {
+      idCliente: 2,
+      comandoPuntoEntrega: {
+          direccion: "Cra 35 sur # 103 aa",
+          municipio: "Bello" 
+      },
+      comandoProductosOrdenados: [
+          { idProducto: 2, cantidad: 1},
+          { idProducto: 9, cantidad: 4}
+      ]
+    };
+  }
+
+  crearListadoClientes() {
+    return ([
+      {
+        id: 1,
+        nombre: "Carlos Manuel Lopez"
+      },
+      {
+        id: 2,
+        nombre: "Luna Maria Debian"
+      },
+      {
+        id: 3,
+        nombre: "Guillermo Celis Mosquera"
+      }
+    ]);
+  }
+
+  crearListadoProductos() {
+    return ([
+      {
+        id: 1,
+        nombre: "Mont Blanc Legend Eau de Parfum",
+        tipoProducto: "PERFUME",
+        valor: 480500.00
+      },
+      {
+        id: 2,
+        nombre: "Casio Retro Iconic Dorado",
+        tipoProducto: "RELOJ",
+        valor: 650000.00
+      }
+    ]);
+  }
+
+  crearListadoPedidosPendientes() {
+    return ([
+      {
+        id: 3,
+        fecha: "2022-05-16",
+        sitioEntrega: "Calle 90 c # 584 - Envigado",
+        valorTotal: 1071525.00,
+        estado: "PENDIENTE"
+      },
+      {
+        id: 6,
+        fecha: "2022-05-16",
+        sitioEntrega: "Diagonal 40 a 35 # 68 - Caldas",
+        valorTotal: 2934890.00,
+        estado: "PENDIENTE"
+      },
+      {
+        id: 9,
+        fecha: "2022-05-16",
+        sitioEntrega: "Calle 40 a su ee - Sabaneta",
+        valorTotal: 1233980.00,
+        estado: "PENDIENTE"
+      },
+      {
+        id: 19,
+        fecha: "2022-05-23",
+        sitioEntrega: "Calle 90 - La Estrella",
+        valorTotal: 1851070.00,
+        estado: "PENDIENTE"
+      }
+    ]);
+  }
+
+  crearListadoPedidosEntregados() {
+    return ([
+      {
+        id: 1,
+        fecha: "2022-05-16",
+        sitioEntrega: "Calle 35 # 42 - 46 - Itagui",
+        valorTotal: 1268960.00,
+        estado: "ENTREGADO"
+      },
+      {
+        id: 2,
+        fecha: "2022-05-16",
+        sitioEntrega: "Diagonal 54 a # 14 - 5 - Medellin",
+        valorTotal: 1839500.00,
+        estado: "ENTREGADO"
+      },
+      {
+        id: 4,
+        fecha: "2022-05-16",
+        sitioEntrega: "Cra 60 dd # 42 - Barbosa",
+        valorTotal: 477520.00,
+        estado: "ENTREGADO"
+      }
+    ]);
+  }
+}
