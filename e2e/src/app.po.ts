1fb016e04e46f37291dd5c054b36cdfb3b7e59cc
CEIBA\pablo.tabares <pablo.tabares@ceiba.com.co>
2022-05-25 16:10:46 -0500

    Se modifican para el uso de los test funcionales con protractor.

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -8,4 +8,20 @@ export class AppPage {
   getTitleText() {
     return element(by.css('app-root h1')).getText() as Promise<string>;
   }
+
+  getTextBySelectorComponentAndTag(selectorComponent: string, tag: string) {
+    return element(by.css(`${selectorComponent} ${tag}`)).getText() as Promise<string>;
+  }
+
+  getListTextBySelectorComponentAndTag(selectorComponent: string, tag: string) {
+    return element.all(by.css(`${selectorComponent} ${tag}`)).getText() as Promise<string>;
+  }
+
+  getTitleTextByName(selectorName: string) {
+    return element(by.className(selectorName)).getText() as Promise<string>;
+  }
+
+  getElementById(selectorId: string) {
+    return element(by.id(selectorId)).isEnabled() as Promise<boolean>;
+  }
 }
