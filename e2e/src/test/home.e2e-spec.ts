dd1945f5a9dcab8079955d4b0259d4212cc4d68c
CEIBA\pablo.tabares <pablo.tabares@ceiba.com.co>
2022-05-26 03:49:44 -0500

    Se crea nuevo test de funcionalidad para el componente home modificado.

--- e2e/src/test/home.e2e-spec.ts
+++ e2e/src/test/home.e2e-spec.ts
@@ -1,24 +1,21 @@
 import { AppPage } from '../app.po';
-//import { HomePage } from '../page/home/home.po';
 import { NavbarPage } from '../page/navbar/navbar.po';
 
 describe('Page Home', () => {
     let page: AppPage;
     let navBar: NavbarPage;
-    //let home: HomePage;
 
     beforeEach(() => {
         page = new AppPage();
         navBar = new NavbarPage();
-        //home = new HomePage();
     });
 
-    it('Deberia entrar a la opcion de Home y comprobar existencia de titulo', () => {
+    it('Deberia entrar a la opcion de Home y comprobar existencia de informacion', () => {
         page.navigateTo();
         navBar.clickBotonHome();
         
-        expect(page.getTextBySelectorComponentAndTag('app-home', 'p')).toEqual('home works!');
+        expect(page.getTextBySelectorComponentAndTag('app-home', 'h1')).toEqual('TRM Dollar');
+        expect(page.getTextBySelectorComponentAndTag('app-home', '#legend-informacion-trm')).toEqual('Información TRM');
+        expect(page.getTextBySelectorComponentAndTag('app-home', '#parrago-informacion')).toContain('$');
     });
-
-
 });
